0161b0219ca6a3d1a8e4470f30a9f8dcfe86cca2
Isa Eken <hello@isaeken.com.tr>
2025-01-30 09:57:37 +0300

    Add console clear command
    Introduced a 'clear' command in the console extension for clearing the terminal output using the system's 'clear' function. Updated the example.bee script to demonstrate the use of this new feature.

--- src/Extensions/ConsoleExtension.php
+++ src/Extensions/ConsoleExtension.php
@@ -47,6 +47,7 @@ class ConsoleExtension implements ExtensionInterface
             },
             'color' => fn ($color, ...$arguments) => $this->color($color, ...$arguments),
             'input' => fn () => readline(),
+            'clear' => fn () => system('clear'),
         ];
     }
 
